eb1ee65fbca380c559281a087f172a1667f68810
Jorge Sala <jorge.e.sala@gmail.com>
2024-11-30 11:18:00 -0500

    added Cors support

--- src/main/java/com/itransition/controller/AuthController.java
+++ src/main/java/com/itransition/controller/AuthController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,7 @@ import com.itransition.service.UserService;
 
 import jakarta.servlet.http.HttpSession;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
